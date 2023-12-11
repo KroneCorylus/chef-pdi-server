0ed8b0c80ad041a688f605c4f608ca848a0b3f6c
atorres <agustin.torres@rs.com.ar>
2023-12-11 09:07:00 -0300

    get secuences

--- pdiserver/services/yaml.py
+++ pdiserver/services/yaml.py
@@ -6,3 +6,9 @@ def get_jobs() -> dict:
     with open(BASE_DIR + "/jobs/jobs.yaml", 'r') as stream:
         data_loaded = yaml.safe_load(stream)
     return data_loaded["jobs"]
+
+
+def get_secuences() -> dict:
+    with open(BASE_DIR + "/jobs/secuences.yaml", 'r') as stream:
+        data_loaded = yaml.safe_load(stream)
+    return data_loaded["secuences"]
