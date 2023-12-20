d2edf45814b139867f191697f2bbbcf9b3d7e801
KroneCorylus <ahustinkrone@gmail.com>
2023-12-20 08:44:44 -0300

    change code directory

--- run-dev.sh
+++ run-dev.sh
@@ -1,6 +1,6 @@
 export PDI_HOME=$(pwd)
 export FILTER_GTK_WARNINGS=true
 export SKIP_WEBKITGTK_CHECK=true
-cd pdiserver
+cd chef
 pip install -r requirements.txt
 flask run --host 0.0.0.0 --port 1882 --reload
