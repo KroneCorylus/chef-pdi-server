ff2e340e93f7d499332fd7eb5857145d8b00a834
KroneCorylus <ahustinkrone@gmail.com>
2023-12-29 07:30:30 -0300

    set token to empty in dev

--- run-dev.sh
+++ run-dev.sh
@@ -1,7 +1,7 @@
 export PDI_HOME=$(pwd)
 export FILTER_GTK_WARNINGS=true
 export SKIP_WEBKITGTK_CHECK=true
-export CHEF_SECRET_KEY=magia
+export CHEF_SECRET_KEY=
 cd chef
 pip install -r requirements.txt
 flask run --host 0.0.0.0 --port 1882 --reload
